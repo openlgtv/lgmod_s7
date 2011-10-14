f6e8151da8f143bbc809c134484f2e1ddc242cf1
mmm4m5m <mmm4m5m>
2011-10-14 14:13:18 +0000

    LGMOD_S7: cross-compile - add DFBTerm

--- cross-compile/dfb-lite.sh
+++ cross-compile/dfb-lite.sh
@@ -13,9 +13,9 @@ d="../../extroot$SUFFIX"; cd "$d"; mkdir -p usr/local; INST_DIR2=$(pwd)
 cd ../..
 if [ "$PLATFORM" = S7 ]; then CC_DIR="$(pwd)/Saturn7/cross-compiler"; CC_PREF=mipsel-linux
 else CC_DIR="$(pwd)/cross-compiler-mipsel"; CC_PREF=mipsel; fi
-if [ "$PLATFORM" = S7 ]; then CC_DFB="$(pwd)/Saturn7/GP2_MSTAR/directFB"; #PKG_CFG="$CC_DIR/mipsel-lg-linux-gnu/sys-root/lib:$CC_DIR/mipsel-lg-linux-gnu/sys-root/usr/lib"
+if [ "$PLATFORM" = S7 ]; then CC_DFB="$(pwd)/Saturn7/GP2_MSTAR/directFB"
 else CC_DFB="$(pwd)/Saturn6/GP1_MSTAR/directFB"; fi
-d=sources; mkdir -p $d; cd $d; SRC_dir=DirectFB-examples-1.2.0; SRC_DIR="$(pwd)/$SRC_dir"
+d=sources; mkdir -p $d; cd $d; SRC_dir=LiTE-0.8.10; SRC_DIR="$(pwd)/$SRC_dir"
 
 echo 'Note: Copy files from LG TV /usr/local/lib to:'
 echo "	$(cd ..; pwd)/DirectFB-LG-usr_local_lib"
@@ -25,7 +25,7 @@ dir=$SRC_dir
 if [ ! -d "$dir" ]; then
 	tar=$dir.tar.gz
 	read -n1 -p "Press Y to download and extract $tar ... " r; echo; [ "$r" = Y ] || exit
-	[ -f "$tar" ] || { wget "http://directfb.org/downloads/Extras/$tar" || exit 3; }
+	[ -f "$tar" ] || { wget "http://directfb.org/downloads/Libs/$tar" || exit 3; }
 	tar -xzf "$tar"
 	exit
 fi
@@ -34,15 +34,14 @@ fi
 CC_BIN="$CC_DIR/bin"
 [ -d "$CC_BIN" ] || { echo "ERROR: $CC_BIN not found."; exit 11; }
 export PATH="$CC_BIN:$PATH"
-export DIRECTFB_CFLAGS="-I$CC_DFB/include -I$CC_DFB/lib -D_REENTRANT"
-export DIRECTFB_LIBS="-L${SRC_DIR%/*}/DirectFB-LG-usr_local_lib -ldirectfb -lfusion -ldirect -lpthread"
+export DFB_CFLAGS="-I$CC_DFB/include -I$CC_DFB/lib -D_REENTRANT"
+export DFB_LIBS="-L${SRC_DIR%/*}/DirectFB-LG-usr_local_lib -ldirectfb -lfusion -ldirect -lpthread"
 
 # config, build
 cd "$SRC_DIR"
 [ "$1" = bash ] && { bash; exit; }
 [ "$1" = noclean ] && shift || { make clean
 	if [ "$PLATFORM" = S7 ]; then
-	     #PKG_CONFIG_LIBDIR="$PKG_CFG" PKG_CONFIG_PATH="$PKG_CFG"
 	     ./configure --host=$CC_PREF; #CFLAGS="-static"
 	else ./configure --host=$CC_PREF; CFLAGS="-static"; fi; }
 [ "$1" = nomake ] && shift || make
