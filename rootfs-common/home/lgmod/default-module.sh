534a87f7a4db01fe562c37f6d2e1c0c9d9c57c58
mmm4m5m <mmm4m5m>
2011-10-16 12:55:54 +0000

    LGMOD_S7: minor change

--- rootfs-common/home/lgmod/default-module.sh
+++ rootfs-common/home/lgmod/default-module.sh
@@ -9,7 +9,6 @@ if [ ! -d /mnt/lg/lginit ]; then # S6 (or S7 with custom kernel)
 	# Asix network chipset module
 	$modprobe asix.ko
 fi
-
 # MCS7830 network chipset module
 $modprobe mcs7830.ko
 #$modprobe mcs7830-lprot.ko
@@ -17,28 +16,30 @@ $modprobe mcs7830.ko
 #$modprobe dm9601-2626.ko
 $modprobe dm9601.ko
 
-# CDROM filesystem modules
-#$modprobe cdrom.ko
-#$modprobe isofs.ko
-#$modprobe sr_mod.ko
+# Ralink based wireless support (Rt2x00, Rt2500, Rt73)
+$modprobe rt2x00usb.ko
+#$modprobe rt2500usb.ko # It crashed with 148f:2573 !
+$modprobe rt73usb.ko
+
 # CIFS/Samba filesystem module with max buffer size 127Kbyte
 #$modprobe cifs.ko CIFSMaxBufSize=130048
 $modprobe cifs.ko
+# NFS filesystem modules
+#$modprobe sunrpc.ko
+#$modprobe lockd.ko
+#$modprobe nfs.ko
+
 # EXT2 filesystem module
 #$modprobe ext2.ko
 # Journaling layer for EXT3 filesystem
 #$modprobe jbd.ko
 # EXT3 filesystem module
 #$modprobe ext3.ko
-# NFS filesystem modules
-#$modprobe sunrpc.ko
-#$modprobe lockd.ko
-#$modprobe nfs.ko
 
-# Ralink based wireless support (Rt2x00, Rt2500, Rt73)
-#$modprobe rt2x00usb.ko
-#$modprobe rt2500usb.ko # It crashed with 148f:2573 !
-#$modprobe rt73usb.ko
+# CDROM filesystem modules
+#$modprobe cdrom.ko
+#$modprobe isofs.ko
+#$modprobe sr_mod.ko
 
 # USB2Serial and mini_fo modules
 #$modprobe usbserial.ko
