2041ccd26deeb3bbddcbd14c9690cdbd27167277
mmm4m5m <mmm4m5m>
2011-10-17 20:20:10 +0000

    LGMOD_S7: mount file systems does load modules it self (no need of modules.sh script); mount started after USB is available - ext3/nfs/cifs modules could be moved to extroot; hotplug - some comments (like Pety do)

--- rootfs-common/home/lgmod/default-module.sh
+++ rootfs-common/home/lgmod/default-module.sh
@@ -1,48 +1,36 @@
 #!/bin/sh
-[ -f /sbin/modprobe ] && modprobe=modprobe || modprobe=insmod
-if [ ! -d /mnt/lg/lginit ]; then # S6 (or S7 with custom kernel)
-	# Core network modules
-	$modprobe mii.ko
-	$modprobe usbnet.ko
-	# Pegasus network chipset module
-	$modprobe pegasus.ko
-	# Asix network chipset module
-	$modprobe asix.ko
-fi
-# MCS7830 network chipset module
-$modprobe mcs7830.ko
-#$modprobe mcs7830-lprot.ko
-# DM9601 network chipset module
-#$modprobe dm9601-2626.ko
-$modprobe dm9601.ko
+[ -d /mnt/lg/lginit ] || $MODPROBE mii.ko; # S6 (or S7 with custom kernel)
+f=usbnet;  [ -d /sys/module/$f ] || $MODPROBE $f.ko; # Core network modules
+f=pegasus; [ -d /sys/module/$f ] || $MODPROBE $f.ko; # Pegasus network chipset module
+f=asix;    [ -d /sys/module/$f ] || $MODPROBE $f.ko; # Asix network chipset module
+$MODPROBE mcs7830.ko; # MCS7830 network chipset module
+$MODPROBE dm9601.ko;  # DM9601 network chipset module
 
 # Ralink based wireless support (Rt2x00, Rt2500, Rt73)
-$modprobe rt2x00usb.ko
-#$modprobe rt2500usb.ko # It crashed with 148f:2573 !
-$modprobe rt73usb.ko
+$MODPROBE rt2x00usb.ko
+#$MODPROBE rt2500usb.ko # It crashed with 148f:2573 !
+$MODPROBE rt73usb.ko
 
 # CIFS/Samba filesystem module with max buffer size 127Kbyte
-#$modprobe cifs.ko CIFSMaxBufSize=130048
-$modprobe cifs.ko
+#$MODPROBE cifs.ko; # CIFSMaxBufSize=130048
 # NFS filesystem modules
-#$modprobe sunrpc.ko
-#$modprobe lockd.ko
-#$modprobe nfs.ko
+#$MODPROBE sunrpc.ko
+#$MODPROBE lockd.ko
+#$MODPROBE nfs.ko
 
 # EXT2 filesystem module
-#$modprobe ext2.ko
-# Journaling layer for EXT3 filesystem
-#$modprobe jbd.ko
-# EXT3 filesystem module
-#$modprobe ext3.ko
+#$MODPROBE ext2.ko
+# EXT3 filesystem modules
+#$MODPROBE jbd.ko
+#$MODPROBE ext3.ko
 
 # CDROM filesystem modules
-#$modprobe cdrom.ko
-#$modprobe isofs.ko
-#$modprobe sr_mod.ko
+#$MODPROBE cdrom.ko
+#$MODPROBE isofs.ko
+#$MODPROBE sr_mod.ko
 
 # USB2Serial and mini_fo modules
-#$modprobe usbserial.ko
-#$modprobe pl2303.ko
-#$modprobe ftdi_sio.ko
-#$modprobe mini_fo.ko
+#$MODPROBE usbserial.ko
+#$MODPROBE pl2303.ko
+#$MODPROBE ftdi_sio.ko
+#$MODPROBE mini_fo.ko
