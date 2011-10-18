721a667bb032229a7e2f58ee376eb2b3e34d3f71
mmm4m5m <mmm4m5m>
2011-10-18 16:05:08 +0000

    LGMOD_S7: "links" install part

--- cross-compile/links.sh
+++ cross-compile/links.sh
@@ -15,8 +15,8 @@ d="../../extroot$SUFFIX"; cd "$d"; mkdir -p usr/local; INST_DIR2=$(pwd)
 cd ../..
 if [ "$PLATFORM" = S7 ]; then CC_DIR="$(pwd)/Saturn7/cross-compiler"; CC_PREF=mipsel-linux
 else CC_DIR="$(pwd)/cross-compiler-mipsel"; CC_PREF=mipsel; fi
-if [ "$PLATFORM" = S7 ]; then CC_DFB="$(pwd)/Saturn7/GP2_MSTAR/directFB"; #PKG_CFG="$CC_DIR/mipsel-lg-linux-gnu/sys-root/lib:$CC_DIR/mipsel-lg-linux-gnu/sys-root/usr/lib"
-else CC_DFB="$(pwd)/Saturn6/GP1_MSTAR/directFB"; fi
+#if [ "$PLATFORM" = S7 ]; then CC_DFB="$(pwd)/Saturn7/GP2_MSTAR/directFB"
+#else CC_DFB="$(pwd)/Saturn6/GP1_MSTAR/directFB"; fi
 d=sources; mkdir -p $d; cd $d; SRC_dir=links-2.3; SRC_DIR="$(pwd)/$SRC_dir"
 
 echo 'Note: Copy files from LG TV /usr/local/lib to:'
@@ -37,21 +37,23 @@ CC_BIN="$CC_DIR/bin"
 [ -d "$CC_BIN" ] || { echo "ERROR: $CC_BIN not found."; exit 11; }
 [ -d "$SRC_DIR" ] || { echo "ERROR: $SRC_DIR not found."; exit 12; }
 export PATH="$CC_BIN:$PATH"
-export CC=$CC_PREF-gcc
 cd "$SRC_DIR"
-export DIRECTFB_CFLAGS="-I$CC_DFB/include -I$CC_DFB/lib -D_REENTRANT"
-export DIRECTFB_LIBS="-L${SRC_DIR%/*}/DirectFB-LG-usr_local_lib -ldirectfb -lfusion -ldirect -lpthread"
+export CC=$CC_PREF-gcc
+#export DIRECTFB_CFLAGS="-I$CC_DFB/include -I$CC_DFB/lib -D_REENTRANT"
+#export DIRECTFB_LIBS="-L${SRC_DIR%/*}/DirectFB-LG-usr_local_lib -ldirectfb -lfusion -ldirect -lpthread"
 
 # config, build
 [ "$1" = bash ] && { bash; exit; }
 [ "$1" = noclean ] && shift || { make clean
 	if [ "$PLATFORM" = S7 ]; then
-	     #PKG_CONFIG_LIBDIR="$PKG_CFG" PKG_CONFIG_PATH="$PKG_CFG"
-	     ./configure --host=$CC_PREF --with-directfb --enable-graphics; #CFLAGS="-static"
+	     ./configure --host=$CC_PREF; #CFLAGS="-static"; #--with-directfb --enable-graphics
 	else ./configure --host=$CC_PREF; CFLAGS="-static"; fi; }
 [ "$1" = nomake ] && shift || make
 
 # install
 [ "$1" = noinstall ] && exit
-read -n1 -p "Press Y to install in $INST_DIR2/usr/local ... " r; echo; [ "$r" = Y ] || exit
-make install DESTDIR="$INST_DIR2"
+dest() { for i in "$@"; do "$CC_BIN/$CC_PREF-strip" --strip-unneeded "$i"; file "$i"; ls -l "$i"; done; }
+read -n1 -p "Press Y to install in $INST_DIR ... " r; echo; [ "$r" = Y ] || exit
+d="$INST_DIR/usr/bin/"
+for i in links; do
+	f="$d$i"; cp -ax $i "$f"; dest "$f"; done
